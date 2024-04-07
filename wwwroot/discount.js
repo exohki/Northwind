45d6d24c48698dfb3268f25b3a1c52f68d00e226
emileemontijo <emontijo@my.wctc.edu>
2024-04-07 13:35:21 -0500

    Updat Toast

--- wwwroot/discount.js
+++ wwwroot/discount.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('discount-row').addEventListener('click', function(e){
         e.preventDefault();
-        bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+        var toast = bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast'));
+        toast.show();
+        playToastSound(); 
     });
+
+    function playToastSound() {
+        var audio = document.getElementById('toastSound');
+        audio.play();
+    }
 });
